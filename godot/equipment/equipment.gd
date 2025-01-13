3c961570c0266022557f597d458c742fbd53f61f
David Hamner <ke7oxh@gmail.com>
2025-01-12 19:05:22 -0800

    1/2 working

--- godot/equipment/equipment.gd
+++ godot/equipment/equipment.gd
@@ -41,26 +41,50 @@ func _physics_process(delta: float) -> void:
 
 func get_base_direction(parent_animation: LPCEnum.LPCAnimation) -> String:
 	match parent_animation:
-		LPCEnum.LPCAnimation.WALK_UP, LPCEnum.LPCAnimation.IDLE_UP, LPCEnum.LPCAnimation.SLASH_UP, LPCEnum.LPCAnimation.THRUST_UP:
+		LPCEnum.LPCAnimation.WALK_UP, LPCEnum.LPCAnimation.IDLE_UP, LPCEnum.LPCAnimation.SLASH_UP, LPCEnum.LPCAnimation.THRUST_UP, LPCEnum.LPCAnimation.SLASH_REVERSE_UP:
 			return "UP"
-		LPCEnum.LPCAnimation.WALK_LEFT, LPCEnum.LPCAnimation.IDLE_LEFT, LPCEnum.LPCAnimation.SLASH_LEFT, LPCEnum.LPCAnimation.THRUST_LEFT:
+		LPCEnum.LPCAnimation.WALK_LEFT, LPCEnum.LPCAnimation.IDLE_LEFT, LPCEnum.LPCAnimation.SLASH_LEFT, LPCEnum.LPCAnimation.THRUST_LEFT, LPCEnum.LPCAnimation.SLASH_REVERSE_LEFT:
 			return "LEFT"
-		LPCEnum.LPCAnimation.WALK_DOWN, LPCEnum.LPCAnimation.IDLE_DOWN, LPCEnum.LPCAnimation.SLASH_DOWN, LPCEnum.LPCAnimation.THRUST_DOWN:
+		LPCEnum.LPCAnimation.WALK_DOWN, LPCEnum.LPCAnimation.IDLE_DOWN, LPCEnum.LPCAnimation.SLASH_DOWN, LPCEnum.LPCAnimation.THRUST_DOWN, LPCEnum.LPCAnimation.SLASH_REVERSE_DOWN:
 			return "DOWN"
-		_:
+		LPCEnum.LPCAnimation.WALK_RIGHT, LPCEnum.LPCAnimation.IDLE_RIGHT, LPCEnum.LPCAnimation.SLASH_RIGHT, LPCEnum.LPCAnimation.THRUST_RIGHT, LPCEnum.LPCAnimation.SLASH_REVERSE_RIGHT:
 			return "RIGHT"
+		_:
+			print("Warning: Unknown animation state: ", parent_animation)
+			return "DOWN"  # Default to DOWN instead of RIGHT
 
 func update_animation(parent_animation: LPCEnum.LPCAnimation) -> void:
 	if not lpc_sprite:
+		print("No lpc_sprite in equipment")
+		return
+	
+	# Get the animated sprite node
+	var animated_sprite: AnimatedSprite2D = null
+	for child in lpc_sprite.get_children():
+		if child is AnimatedSprite2D:
+			animated_sprite = child
+			break
+			
+	if not animated_sprite:
 		return
 		
 	var base_direction = get_base_direction(parent_animation)
-	match base_direction:
-		"UP":
-			lpc_sprite.play(LPCEnum.LPCAnimation.IDLE_UP)
-		"LEFT":
-			lpc_sprite.play(LPCEnum.LPCAnimation.IDLE_LEFT) 
-		"DOWN":
-			lpc_sprite.play(LPCEnum.LPCAnimation.IDLE_DOWN)
-		"RIGHT":
-			lpc_sprite.play(LPCEnum.LPCAnimation.IDLE_RIGHT)
+	var animation_name = ""
+	
+	# Get parent node (should be the player)
+	var player = get_parent().get_parent()
+	if not player:
+		return
+	
+	# Set base movement animation
+	if abs(player.velocity.x) > 1 or abs(player.velocity.y) > 1:
+		animation_name = "WALK_" + base_direction
+		animated_sprite.speed_scale = 1.0  # Normal walking speed
+	else:
+		animation_name = "IDLE_" + base_direction
+		animated_sprite.speed_scale = 1.0  # Normal idle speed
+	
+	print("Equipment base animation: ", animation_name)  # Debug print
+	
+	if animation_name in LPCEnum.LPCAnimation:
+		lpc_sprite.play(LPCEnum.LPCAnimation[animation_name])
--- godot/equipment/wooden_shield.gd
+++ godot/equipment/wooden_shield.gd
@@ -22,18 +22,13 @@ func _ready() -> void:
 		# Force reload of animations
 		lpc_sprite._instantiate()
 		
-		# Get the animated sprite node and setup default state
+		# Get the animated sprite node
 		for child in lpc_sprite.get_children():
 			if child is AnimatedSprite2D:
-				print("Found AnimatedSprite2D node: ", child.name)
-				print("Setting up animation speed")
-				child.speed_scale = 2.0
-				# Connect animation finished signal
+				# Set default speed - walking animation speed
+				child.speed_scale = 1.0
 				child.animation_finished.connect(_on_animation_finished)
 				break
-		
-		# Debug output about sprite setup
-		print("Sprite sheets loaded: ", lpc_sprite.SpriteSheets.size())
 
 func _on_animation_finished() -> void:
 	var player = get_parent().get_parent()
@@ -41,7 +36,6 @@ func _on_animation_finished() -> void:
 		update_animation(player.facing_direction)
 
 func update_animation(parent_animation: LPCEnum.LPCAnimation) -> void:
-	print("Shield update_animation called with: ", LPCEnum.LPCAnimation.keys()[parent_animation])
 	if not lpc_sprite:
 		print("No lpc_sprite in shield")
 		return
@@ -56,42 +50,27 @@ func update_animation(parent_animation: LPCEnum.LPCAnimation) -> void:
 	if not animated_sprite:
 		return
 		
+	var base_direction = get_base_direction(parent_animation)
 	var animation_name = ""
+	
 	# Get parent node (should be the player)
 	var player = get_parent().get_parent()
+	if not player:
+		return
 	
-	# Determine base direction from parent animation
-	var base_direction = ""
-	match parent_animation:
-		LPCEnum.LPCAnimation.WALK_UP, LPCEnum.LPCAnimation.IDLE_UP:
-			base_direction = "UP"
-		LPCEnum.LPCAnimation.WALK_LEFT, LPCEnum.LPCAnimation.IDLE_LEFT:
-			base_direction = "LEFT"
-		LPCEnum.LPCAnimation.WALK_DOWN, LPCEnum.LPCAnimation.IDLE_DOWN:
-			base_direction = "DOWN"
-		_:
-			base_direction = "RIGHT"
-	
-	if player and player.is_blocking:
+	if player.is_blocking:
 		animation_name = "THRUST_" + base_direction
-		animated_sprite.speed_scale = 4.0  # Faster for blocking
-		print("Playing block animation: ", animation_name)
+		animated_sprite.speed_scale = 2.0  # Blocking animation speed
 	else:
 		# Use walking animations for movement, idle for standing still
 		if abs(player.velocity.x) > 1 or abs(player.velocity.y) > 1:
 			animation_name = "WALK_" + base_direction
-			animated_sprite.speed_scale = 2.0
-			print("Playing walk animation: ", animation_name)
+			animated_sprite.speed_scale = 1.0  # Normal walking speed
 		else:
 			animation_name = "IDLE_" + base_direction
-			animated_sprite.speed_scale = 2.0
-			print("Playing idle animation: ", animation_name)
+			animated_sprite.speed_scale = 1.0  # Normal idle speed
+	
+	print("Shield animation: ", animation_name)  # Debug print
 	
 	if animation_name in LPCEnum.LPCAnimation:
 		lpc_sprite.play(LPCEnum.LPCAnimation[animation_name])
-	
-	# Debug the actual sprite state after play
-	print("After play - Animation: ", animation_name)
-	print("After play - Is playing: ", animated_sprite.is_playing())
-	print("After play - Speed scale: ", animated_sprite.speed_scale)
-	print("After play - Base direction: ", base_direction)
